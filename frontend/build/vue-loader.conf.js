45f477feb7025c6c4543c60eee57222ac4f25b6b
Andrey Pastukhov <gmtyllo@gmail.com>
2017-04-20 22:44:10 +1000

    Update build scripts for cssModules and cssSourceMap

--- frontend/build/vue-loader.conf.js
+++ frontend/build/vue-loader.conf.js
@@ -2,11 +2,19 @@ var utils = require('./utils')
 var config = require('../config')
 var isProduction = process.env.NODE_ENV === 'production'
 
+var localIdentName = isProduction
+  ? '[path][name]-[local]-[hash:base64:5]'
+  : '[path][name]-[local]'
+
 module.exports = {
   loaders: utils.cssLoaders({
     sourceMap: isProduction
       ? config.build.productionSourceMap
       : config.dev.cssSourceMap,
     extract: isProduction
-  })
+  }),
+  cssModules: {
+    localIdentName,
+    camelCase: true
+  }
 }
